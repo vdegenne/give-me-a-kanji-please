c4adf2bf58a143847ee4af2d5ce5614ad39b8838
Valentin Degenne <vdegenne@gmail.com>
2025-02-24 19:29:03 +0100

    simple keys

--- src/global-listeners.ts
+++ src/global-listeners.ts
@@ -1,15 +1,27 @@
 import {getThemeStore} from './imports.js';
+import {store} from './store.js';
 
-window.addEventListener('keydown', async (e) => {
-	if (e.altKey || e.ctrlKey) {
+window.addEventListener('keydown', async (event: KeyboardEvent) => {
+	// console.log(event);
+	if (event.altKey || event.ctrlKey) {
 		return;
 	}
-	const target = e.composedPath()[0] as Element;
+
+	const target = event.composedPath()[0] as Element;
 	if (['TEXTAREA', 'INPUT'].includes(target.tagName)) {
 		return;
 	}
-	if (e.key === 'd') {
-		(await getThemeStore()).toggleMode();
+
+	switch (event.key) {
+		case 'd':
+			(await getThemeStore()).toggleMode();
+			break;
+		case ' ':
+			store.toggleQuizState();
+			break;
+		case 'ArrowRight':
+			store.giveMeAKanjiPlease();
+			break;
 	}
 });
 
